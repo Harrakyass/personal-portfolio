24546c7b81716eb1c8993f696e1fd689520286a8
harrakyass <yassir.ch213@gmail.com>
2025-01-01 18:30:13 +0100

    additional add

--- script.js
+++
@@ -1,5 +0,0 @@
-const turnVisible = document.getElementById('show-contact');
-const invisibleForm = document.querySelector('.invisible-contact');
-turnVisible.addEventListener('click', () =>{
-    invisibleForm.style.display = 'block';
-})
